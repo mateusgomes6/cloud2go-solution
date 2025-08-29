af4cd8c28466e6bbc0dbd47816d73a5794781ceb
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-08-29 14:24:02 -0300

    feat: add config

--- api_prediction.py
+++ api_prediction.py
@@ -7,3 +7,10 @@ from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'csv'}
+MODEL_PATH = 'modelo_treinado.pkl'
+PREPROCESSING_INFO_PATH = 'preprocessing_info.json'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
